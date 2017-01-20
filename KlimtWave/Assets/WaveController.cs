d50dbac8e4bb5c52568511d2bf9356e73f33c841
producto <josiah.jordan@gmail.com>
2017-01-20 22:27:34 +1300

    Successfully implemented first pass of wave physics.

--- KlimtWave/Assets/WaveController.cs
+++ KlimtWave/Assets/WaveController.cs
@@ -14,18 +14,41 @@ public class WaveController : MonoBehaviour
 	{
         var startPosition = Waves.transform.position;
         startPosition.x -= 960F;
-        startPosition.y -= 540F;
+        startPosition.y -= 1350F;
         var startRotation = Waves.transform.rotation;
 
         for (int i = 0; i < 15; i++)
-	    {
-	        waveColumns.Add(GameObject.Instantiate(WaveColumnPrefab, startPosition, startRotation));
+        {
+            var newWave = GameObject.Instantiate(WaveColumnPrefab, startPosition, startRotation);
+            //if (i == 0) newWave.GetComponent<Rigidbody2D>().isKinematic = true;
+            if (i != 0)
+            {
+                newWave.GetComponent<SpringJoint2D>().connectedBody = waveColumns[i - 1].GetComponent<Rigidbody2D>();
+            }
+            waveColumns.Add(newWave);
+
 	        startPosition.x += 128F;
 	    }
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        // Animate...EVERYTHING
+        /*foreach (var waveColumn in waveColumns)
+	    {
+	        
+	    }*/
+
+        HandleInput();
+    }
+
+
+
+    private void HandleInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            waveColumns[0].GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1000F));
+        }
+    }
 }
