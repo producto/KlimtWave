abf832f6c80dd88362ac5ac241969a34830db4b2
producto <josiah.jordan@gmail.com>
2017-01-20 21:15:56 +1300

    Added wave generation prefabs and script.

---
+++ KlimtWave/Assets/WaveController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour
+{
+    public GameObject Waves;
+    public GameObject WaveColumnPrefab;
+    private List<GameObject> waveColumns = new List<GameObject>();
+
+	// Use this for initialization
+	void Start ()
+	{
+        var startPosition = Waves.transform.position;
+        startPosition.x -= 960F;
+        startPosition.y -= 540F;
+        var startRotation = Waves.transform.rotation;
+
+        for (int i = 0; i < 15; i++)
+	    {
+	        waveColumns.Add(GameObject.Instantiate(WaveColumnPrefab, startPosition, startRotation));
+	        startPosition.x += 128F;
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
