3331b5dcb787babeee648723a85a97852e113425
Edward Dalley <edwarddalley@Edwards-Mac-mini.local>
2017-01-21 13:59:11 +1300

    The Force propogation wave style

--- KlimtWave/Assets/WaveController.cs
+++ KlimtWave/Assets/WaveController.cs
@@ -9,9 +9,12 @@ public class WaveController : MonoBehaviour
 	public GameObject WaveColumnPrefab;
 	private List<GameObject> waveColumns = new List<GameObject> ();
 
+	private float mousePos;
+
 	// Use this for initialization
 	void Start ()
 	{
+		mousePos = 0F;
 		var startPosition = Waves.transform.position;
 		startPosition.x -= 960F;
 		startPosition.y -= 1350F;
@@ -27,8 +30,8 @@ public class WaveController : MonoBehaviour
 			startPosition.x += 128F;
 		}
 
-		waveColumns [0].GetComponent<SpringJoint2D> ().connectedAnchor = new Vector2 (0, -1080 - 540 / 2);
-		waveColumns [0].GetComponent<SpringJoint2D> ().distance = 500;
+//		waveColumns [0].GetComponent<SpringJoint2D> ().connectedAnchor = new Vector2 (0, -1080 - 540 / 2);
+//		waveColumns [0].GetComponent<SpringJoint2D> ().distance = 0;
 	}
 
 	void FixedUpdate ()
@@ -39,17 +42,44 @@ public class WaveController : MonoBehaviour
 
 	private void UpdateWaves ()
 	{
-		for (int i = waveColumns.Count - 1; i > 0; i--) {
-			waveColumns[i].transform.position = new Vector3(waveColumns[i].transform.position.x,
-                                                            waveColumns[i - 1].transform.position.y,
-                                                            waveColumns[i].transform.position.z);
+		for (int i = 0; i < waveColumns.Count; i++) {
+			CalculateForceVector (i);
 		}
+	}
+
+
+	private void CalculateForceVector(int i){
+		var balancingConstant = 1000F;
+		var dampingConstant = -100F;
+		var balanceY = -1350F;
+		var transmittanceConstant = 8000F;
+		var controlConstant = 2000000F; 
+		var previousPos = balanceY;
+
+		if (i > 0) {
+			previousPos = waveColumns [i - 1].GetComponent<Rigidbody2D> ().position.y;
+			controlConstant = controlConstant/(i+1);
+		} else {
+			transmittanceConstant = 0;
+		}
+		var body = waveColumns [i].GetComponent<Rigidbody2D> ();
+
+		body.AddForce (new Vector2 (0, (balanceY - body.position.y) * balancingConstant + 
+			
+			(previousPos - body.position.y) * transmittanceConstant + 
+			body.velocity.y * dampingConstant +
+			(mousePos)*controlConstant ));
 	}
 
 	private void HandleInput ()
 	{
-		if (Input.GetKey (KeyCode.Space)) {
-			waveColumns [0].GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, 100000F));
-		}
+		
+//		if (Input.GetKey (KeyCode.Space)) {
+//			waveColumns [0].GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, 100000F));
+//		}
+
+
+		mousePos = Input.GetAxis("Mouse Y");
+
 	}
 }
