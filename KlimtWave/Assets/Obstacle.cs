498e077f1636eebf47cc6576bc9a04a04e855079
producto <josiah.jordan@gmail.com>
2017-01-21 21:46:07 +1300

    Started work on Fish controller script.

--- KlimtWave/Assets/Obstacle.cs
+++ KlimtWave/Assets/Obstacle.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour {
 
-    public GameObject Cat;
+    private GameObject player;
+    private Animator obstacleAnimator;
 
     // Use this for initialization
     void Start () {
-	}
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
+        obstacleAnimator = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-    void OnCollisionEnter2D(Collision2D collider)
+    void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.tag == "Player")
+        {
+            Debug.Log("hit player");
+            obstacleAnimator.SetInteger("FishAnimationState", 1);
+        }
+        /*if (collider.gameObject.tag == "Player")
+        {
             collider.gameObject.SendMessage("Death");
+        }*/
     }
 }
