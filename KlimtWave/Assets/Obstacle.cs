2359eca0ac96818f12811cba4b2791f87e6b5f88
producto <josiah.jordan@gmail.com>
2017-01-21 16:26:33 +1300

    Added "Obstacle" and "Player" scripts, and set up system for basic Death state when hitting an obstacle.

---
+++ KlimtWave/Assets/Obstacle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+    public GameObject Cat;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+            collider.gameObject.SendMessage("Death");
+    }
+}
---
+++ KlimtWave/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Death()
+    {
+        Debug.Log("Died");
+        GetComponent<CircleCollider2D>().enabled = false;
+
+        var particleSystem = transform.FindChild("Blood");
+        Debug.Log(particleSystem != null);
+
+        particleSystem.gameObject.SetActive(true);
+    }
+}
