7e351bb1944867b93cc8b54a7ba4fc7852950c74
Stijn Seghers <stijnseghers@gmail.com>
2017-01-22 16:27:31 +1300

    Force fullscreen 1920x1080

--- KlimtWave/Assets/CameraController.cs
+++ KlimtWave/Assets/CameraController.cs
@@ -13,6 +13,7 @@ public class CameraController : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
+		Screen.SetResolution (1920, 1080, true);
 	}
 	
 	// Update is called once per frame
