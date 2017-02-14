2518fc5d41ff87239a243375f507d11a756906a8
Stijn Seghers <stijnseghers@gmail.com>
2017-02-14 20:37:33 +1300

    Move cat more to the left

--- KlimtWave/Assets/CameraController.cs
+++ KlimtWave/Assets/CameraController.cs
@@ -9,6 +9,7 @@ public class CameraController : MonoBehaviour
 	
 	public GameObject cat;
 	public GameObject block;
+	public float dist_cat_cam;
 
 	// Use this for initialization
 	void Start ()
@@ -19,12 +20,10 @@ public class CameraController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		block.transform.position = new Vector3 (transform.position.x - 960, 0, 0);
-
-		if (this.transform.position.x < cat.transform.position.x) {
-
-			transform.Translate (new Vector3 ((cat.transform.position.x - this.transform.position.x), 0F));
+		if (this.transform.position.x - cat.transform.position.x < dist_cat_cam) {
+			transform.Translate (new Vector3 (cat.transform.position.x - this.transform.position.x + dist_cat_cam, 0F));
 		}
+		block.transform.position = new Vector3 (transform.position.x - 960, 0, 0);
 	}
 }
 
